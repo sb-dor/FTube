84598119d41c3e1602bed639ad6db00281905dd6
sarbad0r <avaz.shams.2002@gmail.com>
2023-12-10 21:38:09 +0500

    1

--- lib/utils/view_format_helper/view_format_helper.dart
+++ lib/utils/view_format_helper/view_format_helper.dart
@@ -1,7 +1,10 @@
 abstract class ViewFormatHelper {
   static String viewsFormatNumbers(int? number) {
     if (number == null) return '';
-    if (number >= 1000000) {
+    if (number >= 1000000000) {
+      double result = number / 1000000000;
+      return "${result.toStringAsFixed(result.truncateToDouble() == result ? 0 : 1)} млрд.";
+    } else if (number >= 1000000) {
       double result = number / 1000000.0;
       return '${result.toStringAsFixed(result.truncateToDouble() == result ? 0 : 1)} млн.';
     } else if (number >= 1000) {
