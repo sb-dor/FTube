efcb36f94a42f1a7b8ae2cacc8f4b359f39a3e16
sarbad0r <avaz.shams.2002@gmail.com>
2024-01-08 23:08:34 +0500

    fixing problem with breaking video download from network

--- lib/utils/http_downloader/http_downloader_helper.dart
+++ lib/utils/http_downloader/http_downloader_helper.dart
@@ -3,52 +3,59 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-
 typedef DownloadingProgress = void Function(
-  int total, /// [total mb]
-  int downloading, /// [total downloaded]
-  double progress, /// [percentage of downloaded]
+  int total,
+
+  /// [total mb]
+  int downloading,
+
+  /// [total downloaded]
+  double progress,
+
+  /// [percentage of downloaded]
 );
 
 abstract class HttpDownloaderHelper {
   static Future<Uint8List> download(String url, DownloadingProgress downloadingProgress) async {
     final completer = Completer<Uint8List>();
-    final client = http.Client();
-    final request = http.Request("GET", Uri.parse(url));
-    final response = client.send(request);
 
-    int downloadingBytes = 0;
-    List<List<int>> chunkList = [];
+    try {
+      final client = http.Client();
+      final request = http.Request("GET", Uri.parse(url));
+      final response = client.send(request);
 
-    response.asStream().listen((http.StreamedResponse streamedResponse) {
-      streamedResponse.stream.listen(
-        (chunk) {
-          final int contentLength = streamedResponse.contentLength ?? 0;
-          final progress = (downloadingBytes / contentLength) * 100;
-          downloadingProgress(contentLength, downloadingBytes, progress);
+      int downloadingBytes = 0;
+      List<List<int>> chunkList = [];
 
-          chunkList.add(chunk);
-          downloadingBytes += chunk.length;
-        },
-        onDone: () {
-          final int contentLength = streamedResponse.contentLength ?? 0;
-          final progress = (downloadingBytes / contentLength) * 100;
-          downloadingProgress(contentLength, downloadingBytes, progress);
+      response.asStream().listen((http.StreamedResponse streamedResponse) {
+        streamedResponse.stream.listen(
+          (chunk) {
+            final int contentLength = streamedResponse.contentLength ?? 0;
+            final progress = (downloadingBytes / contentLength) * 100;
+            downloadingProgress(contentLength, downloadingBytes, progress);
 
-          int start = 0;
-          final bytes = Uint8List(contentLength);
+            chunkList.add(chunk);
+            downloadingBytes += chunk.length;
+          },
+          onDone: () {
+            final int contentLength = streamedResponse.contentLength ?? 0;
+            final progress = (downloadingBytes / contentLength) * 100;
+            downloadingProgress(contentLength, downloadingBytes, progress);
 
-          for (var each in chunkList) {
-            bytes.setRange(start, start + each.length, each);
-            start += each.length;
-          }
+            int start = 0;
+            final bytes = Uint8List(contentLength);
 
-          completer.complete(bytes);
-        },
-        onError: (error) => completer.completeError(error),
-      );
-    });
+            for (var each in chunkList) {
+              bytes.setRange(start, start + each.length, each);
+              start += each.length;
+            }
 
+            completer.complete(bytes);
+          },
+          onError: (error) => completer.completeError(error),
+        );
+      });
+    } catch (_) {}
     return completer.future;
   }
 }
