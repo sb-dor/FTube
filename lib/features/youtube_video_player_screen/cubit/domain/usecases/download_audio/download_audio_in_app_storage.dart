862938cae6eb6d8b0c6dac5299678fb9e8d4a0d5
sarbador <avaz.shams.2002@gmail.com>
2024-01-31 22:29:46 +0500

    making downloading mush more better

--- lib/features/youtube_video_player_screen/cubit/domain/usecases/download_audio/download_audio_in_app_storage.dart
+++ lib/features/youtube_video_player_screen/cubit/domain/usecases/download_audio/download_audio_in_app_storage.dart
@@ -1,9 +1,25 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:youtube/features/youtube_video_player_screen/cubit/domain/repository/downloading_audio_repository/downloading_audio_repository.dart';
+import 'package:youtube/utils/reusable_global_functions.dart';
 
 class DownloadAudioInAppStorage implements DownloadingAudioRepository {
+  final ReusableGlobalFunctions _reusableGlobalFunctions = ReusableGlobalFunctions.instance;
+
   @override
-  Future<void> download(List<int>? downloadData) {
-    // TODO: implement download
-    throw UnimplementedError();
+  Future<void> download(List<int>? downloadData) async {
+    var getExternalStoragePath = await getExternalStorageDirectory();
+
+    var dateTimeForVideoName = DateTime.now();
+
+    var pathOfVideo = '${getExternalStoragePath?.path}/'
+        '${_reusableGlobalFunctions.removeSpaceFromStringForDownloadingVideo(dateTimeForVideoName.toString())}.mp3';
+
+    File fileForSaving = File(pathOfVideo);
+
+    if (downloadData != null && downloadData.isNotEmpty) {
+      fileForSaving.writeAsBytesSync(downloadData);
+    }
   }
 }
