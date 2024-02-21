c4f63564460f86e7c71a9dce98143e6652b70ac0
sarbador <avaz.shams.2002@gmail.com>
2024-02-21 07:19:23 +0500

    making history database

---
+++ lib/features/library_screen/presentation/pages/main_library_page/widgets/history_widgets/error_history_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ErrorHistoryWidget extends StatelessWidget {
+  const ErrorHistoryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
---
+++ lib/features/library_screen/presentation/pages/main_library_page/widgets/history_widgets/loaded_history_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoadedHistoryWidget extends StatelessWidget {
+  const LoadedHistoryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
