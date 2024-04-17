a867f60ec91f43cd6ee419dc48c317beb1f74c4b
sarbador <avaz.shams.2002@gmail.com>
2024-04-17 22:03:39 +0500

    added full screen for video

--- lib/features/youtube_video_player_screen/presentation/widgets/video_full_screen_widget/video_full_screen_widget.dart
+++ lib/features/youtube_video_player_screen/presentation/widgets/video_full_screen_widget/video_full_screen_widget.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:video_player/video_player.dart';
+import 'package:youtube/features/youtube_video_player_screen/cubit/youtube_video_cubit.dart';
+import 'package:youtube/features/youtube_video_player_screen/presentation/widgets/video_player_widgets/black_with_opacity_background.dart';
+import 'package:youtube/features/youtube_video_player_screen/presentation/widgets/video_player_widgets/last_next_stop_play_widget.dart';
+import 'package:youtube/features/youtube_video_player_screen/presentation/widgets/video_player_widgets/video_dutaion_information.dart';
+import 'package:youtube/features/youtube_video_player_screen/presentation/widgets/video_player_widgets/video_player_widget.dart';
+import 'package:youtube/features/youtube_video_player_screen/presentation/widgets/video_player_widgets/video_settings_button.dart';
 
 class VideoFullScreenWidget extends StatefulWidget {
   const VideoFullScreenWidget({super.key});
@@ -10,6 +18,37 @@ class VideoFullScreenWidget extends StatefulWidget {
 class _VideoFullScreenWidgetState extends State<VideoFullScreenWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Builder(
+      builder: (context) {
+        final youtubeVideoCubit = context.watch<YoutubeVideoCubit>();
+
+        // data
+
+        final youtubeVideoStateModel = youtubeVideoCubit.state.youtubeVideoStateModel;
+
+        return Scaffold(
+          body: RotatedBox(
+            quarterTurns: 1,
+            child: Container(
+              color: Colors.amber,
+              width: MediaQuery.of(context).size.height,
+              height: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  const VideoPlayerWidget(),
+                  const BlackWithOpacityBackground(),
+                  if (youtubeVideoStateModel.clickedUpOnVideo)
+                    const VideoDurationInformation(
+                      fullScreen: false,
+                    ),
+                  if (youtubeVideoStateModel.clickedUpOnVideo) const LastNextStopPlayWidget(),
+                  if (youtubeVideoStateModel.clickedUpOnVideo) const VideoSettingsButton(),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
--- lib/features/youtube_video_player_screen/presentation/widgets/video_player_widgets/video_dutaion_information.dart
+++ lib/features/youtube_video_player_screen/presentation/widgets/video_player_widgets/video_dutaion_information.dart
@@ -4,12 +4,18 @@ import 'package:video_player/video_player.dart';
 import 'package:youtube/animations/fade_animation.dart';
 import 'package:youtube/features/youtube_video_player_screen/cubit/youtube_video_cubit.dart';
 import 'package:youtube/features/youtube_video_player_screen/cubit/youtube_video_states.dart';
+import 'package:youtube/features/youtube_video_player_screen/presentation/widgets/video_full_screen_widget/video_full_screen_widget.dart';
 import 'package:youtube/utils/duration_helper/duration_helper.dart';
 import 'package:youtube/widgets/text_widget.dart';
 import 'package:youtube/x_injection_containers/injection_container.dart';
 
 class VideoDurationInformation extends StatelessWidget {
-  const VideoDurationInformation({Key? key}) : super(key: key);
+  final bool fullScreen;
+
+  const VideoDurationInformation({
+    Key? key,
+    this.fullScreen = true,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -42,21 +48,35 @@ class VideoDurationInformation extends StatelessWidget {
               const SizedBox(width: 10),
               Row(children: [
                 TextWidget(
-                  text:
-                      locator<DurationHelper>().getFromDuration(currentState.playerController!.value.duration),
+                  text: locator<DurationHelper>()
+                      .getFromDuration(currentState.playerController!.value.duration),
                   size: 13,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
-                // IconButton(
-                //     style: ButtonStyle(
-                //         overlayColor: MaterialStatePropertyAll(Colors.white.withOpacity(0.3))),
-                //     onPressed: () => [],
-                //     icon: const Icon(
-                //       Icons.fullscreen,
-                //       size: 28,
-                //       color: Colors.white,
-                //     ))
+                IconButton(
+                  style: ButtonStyle(
+                      overlayColor: MaterialStatePropertyAll(Colors.white.withOpacity(0.3))),
+                  onPressed: () {
+                    if (fullScreen) {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (context, animation1, animation2) => VideoFullScreenWidget(),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero,
+                        ),
+                      );
+                    } else {
+                      Navigator.pop(context);
+                    }
+                  },
+                  icon: const Icon(
+                    Icons.fullscreen,
+                    size: 28,
+                    color: Colors.white,
+                  ),
+                )
               ])
             ]),
           ));
